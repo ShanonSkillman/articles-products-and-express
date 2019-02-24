8bef3b093dfea32d9886d761d95b3a0e4e591583
Djavan Cackley <djavancackley@Djavans-MacBook-Pro.local>
2019-02-24 12:57:02 -1000

    products and article routes working next dbs

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const articles = require('./routes/articles')
+const products = require('./routes/products')
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.engine('hbs', hbs({
+    defaultLayout: 'home',
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs');
+
+app.use('/articles', articles)
+app.use('/products', products)
+
+app.listen(8080, () => {
+    console.log("app listening on port 8080")
+});
